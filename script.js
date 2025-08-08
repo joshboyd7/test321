834b0275aa040abf56ea2b5177093647decb53a3
joshboyd7 <joshboyd@wharton.upenn.edu>
2025-08-08 11:40:15 -0400

    acs data name

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const FILES = {
   county_irs: "data/irs_county_pagerank_combined.geojson",
   axel_national: "data/axel_national.geojson",
   metro_irs: "data/irs_pagerank_combined.geojson",
-  metro_acs: "data/acs_pagerank_combined.geojson",
+  metro_acs: "data/acs_rolling5_pagerank.geojson",
   acs_rolling5: "data/acs_rolling5_pagerank.geojson" 
 };
 
